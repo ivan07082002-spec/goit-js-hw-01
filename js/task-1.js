2c3ce902397564c5604be0a128bd4c3b1ddbc446
Ivan Ursul <ivan07082002@gmail.com>
2026-04-03 17:58:58 +0200

    my first hw-1

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function makeTransaction(quantity, pricePerDroid) {
+  //....
+}
+
+console.log(makeTransaction(5, 3000)); // "Ви замовили 5 дроїдів вартістю 15000 кредитів!"
+console.log(makeTransaction(3, 1000)); // "Ви замовили 3 дроїдів вартістю 3000 кредитів!"
+console.log(makeTransaction(10, 500)); // "Ви замовили 10 дроїдів вартістю 5000 кредитів!"
